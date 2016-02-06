b937076658af32141c5803fef2ffe827ae1b1afb
Gromych <gromych@yandex-team.ru>
2016-02-06 22:05:46 +0300

    makes tests atomic, adds test for splice bug

--- test/pubsub.js
+++ test/pubsub.js
@@ -4,32 +4,46 @@ var sinon = require('sinon');
 
 describe('PubSub', function () {
     var pubsub;
-    var cbTest = sinon.spy();
-    var cbTest2 = sinon.spy();
+    var cbTest;
+    var cbTest2;
+    var cbTest3;
 
-    before(function () {
+    beforeEach(function () {
         pubsub = new PubSub();
+        cbTest = sinon.spy();
+        cbTest2 = sinon.spy();
+        cbTest3 = sinon.spy();
+
         pubsub.on('test', cbTest);
+        pubsub.on('test', cbTest3);
         pubsub.once('test-2', cbTest2);
-        pubsub.emit('test', 'Argument1', 'Argument2');
     });
 
     it('calls callback only once', function () {
+        pubsub.emit('test');
         expect(cbTest.callCount).to.equal(1);
     });
 
     it('doesn\'t call callback for another event', function () {
+        pubsub.emit('test');
         expect(cbTest2.callCount).to.equal(0);
     });
 
     it('passes correct arguments to the callback', function () {
+        pubsub.emit('test', 'Argument1', 'Argument2');
         expect(cbTest.getCall(0).args).to.deep.equal(['Argument1', 'Argument2']);
     });
 
     it('doesn\'t call callback after unsubscription', function () {
         pubsub.off('test', cbTest);
         pubsub.emit('test');
-        expect(cbTest.callCount).to.equal(1);
+        expect(cbTest.callCount).to.equal(0);
+    });
+
+    it('continues to call other subscribers after another\'s unsubscription', function () {
+        pubsub.off('test', cbTest);
+        pubsub.emit('test');
+        expect(cbTest3.callCount).to.equal(1);
     });
 
     it('calls subscribed via `once` callback only once', function () {
@@ -39,6 +53,7 @@ describe('PubSub', function () {
     });
 
     it('passes arguments to the `once` callback correctly', function () {
+        pubsub.emit('test-2', 1);
         expect(cbTest2.getCall(0).args).to.deep.equal([1]);
     });
 });
